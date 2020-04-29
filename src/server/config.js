69341e84038432eee392ed2b314aefeabd7d1440
Tharles Andrade <irtharles@gmail.com>
2020-04-28 23:08:13 -0300

    Add env file

--- src/server/config.js
+++ src/server/config.js
@@ -1,56 +1,56 @@
-var appRoot = require('app-root-path');
+const appRoot = require('app-root-path');
+const dotenv  = require('dotenv');
+
+const result = dotenv.config();
+if (result.error) {
+	throw result.error;
+}
+const { parsed: env} = result;
 
 module.exports = function (app) {
 
-	var appProducao = '/STORAGE/dpat-files';
+	var appProducao = env.APP_PRODUCAO;
 
 	var config = {
 		"appRoot": appRoot,
-		"clientDir": appRoot + "/../hotsite/",
-		// "clientDir": appRoot + "/../client/dist/",
-		"langDir": appRoot + "/lang",
-		"logDir": appRoot + "/log/",
-		"tmp": appRoot + "/tmp/",
-		"fieldDataDir": appRoot + '/media/campo/',
-		"uploadDataDir": appRoot + "/uploads/",
-		"downloadDataDir": appProducao + "/download-dpat/",
+		"clientDir": appRoot + env.CLIENT_DIR,
+		"langDir": appRoot + env.LANG_DIR,
+		"logDir": appRoot + env.LOG_DIR,
+		"tmp": appRoot + env.TMP,
+		"fieldDataDir": appRoot + env.FIELD_DATA_DIR,
+		"uploadDataDir": appRoot + env.UPLOAD_DATA_DIR,
+		"downloadDataDir": appProducao + env.DOWNLOAD_DATA_DIR,
 		"pg": {
-			"user": 'postgres',
-			// "user": 'fip_cerrado',
-			"host": '10.0.0.14',
-			// "host": 'localhost',
-			"database": 'fip_cerrado',
-			"password": 'postgres',
-			// "password": 'fip_cerrado123',
-			"port": 5432,
-			// "port": 5433,
-			"debug": true
+			"user": env.PG_USER,
+			"host": env.PG_HOST,
+			"database": env.PG_DATABASE,
+			"password": env.PG_PASSWORD,
+			"port": env.PG_PORT,
+			"debug": env.PG_DEBUG
 		},
-		"port": 3000,
-		// "ows_host" : 'http://localhost:5001',
-		"ows_host": 'http://ows.lapig.iesa.ufg.br',
-		"ows": "http://ows.lapig.iesa.ufg.br",
-		"lapig-maps": 'http://maps.lapig.iesa.ufg.br/time-series/MOD13Q1_NDVI/values?'
+		"port": env.PORT,
+		"ows_host": env.OWS_HOST,
+		"ows": env.OWS,
+		"lapig-maps": env.LAPIG_MAPS
 
 	};
 
 	if (process.env.NODE_ENV == 'prod') {
-		config['dbpath'] = "/data/catalog/Ocultos/d-pat.sqlite"
+		config['dbpath'] = env.DB_PATH
 		config["pg"] = {
-			"user": 'fip_cerrado',
-			"host": '172.18.0.4',
-			"database": 'fip_cerrado',
-			"password": 'fip_cerrado123',
-			"port": 5432,
-			"debug": true
-		}
-		config["clientDir"] = appRoot + "/../client/dist/lapig-dpat/"
-		config["ows_host"] = "http://ows.lapig.iesa.ufg.br"
-		config["fieldDataDir"] = appProducao + "/campo-dpat/"
-		config["uploadDataDir"] = appProducao + "/upload-dpat/"
+			"user": env.PG_USER,
+			"host": env.PG_HOST,
+			"database": env.PG_DATABASE,
+			"password": env.PG_PASSWORD,
+			"port": env.PG_PORT,
+			"debug": env.PG_DEBUG
+		};
+		config["clientDir"] = appRoot +  env.CLIENT_DIR;
+		config["ows_host"] =  env.OWS_HOST;
+		config["fieldDataDir"] = appProducao +  env.FIELD_DATA_DIR;
+		config["uploadDataDir"] = appProducao +  env.UPLOAD_DATA_DIR
 
 	}
 	
 	return config;
-
 }
