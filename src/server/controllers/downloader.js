bfef5ad43a2afe5fe174eb613b2d9c9972090402
luizmlpascoal <luizmlpascoal@gmail.com>
2020-05-07 15:24:43 -0300

    up teste on csv

--- src/server/controllers/downloader.js
+++ src/server/controllers/downloader.js
@@ -45,9 +45,9 @@ module.exports = function (app) {
         let time   = request.body.times;
 
         let mapper = {
-            "bi_ce_prodes_desmatamento_100_fip" : "select origin_gid,view_date,source, cd_geocmu, uf, sucept_desmat, bfm_pct, year, classefip, sum(areamunkm) from prodes_cerrado",
-            "bi_ce_deter_desmatamento_100_fip" : "select origin_gid,view_date,source, cd_geocmu, uf, sucept_desmat, bfm_pct, date_part('year', deter_cerrado.view_date) AS year, classefip, sum(areamunkm) from deter_cerrado",
-            "bi_ce_prodes_antropico_100_fip" : "select origin_gid,view_date,source, cd_geocmu, uf, sucept_desmat, bfm_pct, year, classefip, sum(areamunkm) from prodes_cerrado"
+            "bi_ce_prodes_desmatamento_100_fip" : "select gid,view_date, cd_geocmu, uf, sucept_desmat, bfm_pct, year, classefip, sum(areamunkm) from prodes_cerrado",
+            "bi_ce_deter_desmatamento_100_fip" : "select gid,view_date, cd_geocmu, uf, sucept_desmat, bfm_pct, date_part('year', deter_cerrado.view_date) AS year, classefip, sum(areamunkm) from deter_cerrado",
+            "bi_ce_prodes_antropico_100_fip" : "select gid,view_date, cd_geocmu, uf, sucept_desmat, bfm_pct, year, classefip, sum(areamunkm) from prodes_cerrado"
         }
 
         var sqlQuery = mapper[layer];
@@ -69,7 +69,7 @@ module.exports = function (app) {
             sqlQuery += " AND " + region.value
         }
 
-        sqlQuery+= " GROUP BY 1,2,3,4,5,6,7,8,9"
+        sqlQuery+= " GROUP BY 1,2,3,4,5,6,7,8"
 
 
         client.query(sqlQuery, (err, rows) => {
@@ -78,7 +78,7 @@ module.exports = function (app) {
                 response.end()
             } else {
 
-                var output = file+".csv";
+                var output = layer+".csv";
                 var csv = json2csv(rows.rows);
 
                 fs.writeFile(output, csv, function(err) {
