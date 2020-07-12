0906f3797c82f02f50d198255c7030c2d192789a
Tharles Andrade <irtharles@gmail.com>
2020-07-12 03:41:38 -0300

    update text of project.

--- src/client/src/app/views/project/project.component.ts
+++ src/client/src/app/views/project/project.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
-export class ProjectComponent implements OnInit {
 
+export class ProjectComponent implements OnInit {
+  windowScrolled:boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener("click", ["$event"])
+  goToSection(id){
+    try {
+      event.stopPropagation();
+      let el = document.querySelector('#' + id).scrollIntoView({ behavior: 'smooth', block: 'nearest'});
+    } catch (e) { }
   }
 
 }
