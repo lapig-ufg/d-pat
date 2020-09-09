cacdafaebbcb5cfdff2b252f9612292c92bb9081
luizmlpascoal <luizmlpascoal@gmail.com>
2020-09-09 00:33:47 -0300

    up cpu cores

--- src/server/app-dpat-cluster.js
+++ src/server/app-dpat-cluster.js
@@ -1,23 +1,23 @@
 var cluster = require('cluster');
 var http = require('http');
-var numCPUs = require('os').cpus().length;
-var numCPUs = 16;
+// var numCPUs = require('os').cpus().length;
+var numCPUs = 24;
 
 if (cluster.isMaster) {
-  if(process.env.NODE_ENV == 'dev') {
+  if (process.env.NODE_ENV == 'dev') {
     var numCPUs = 2
   }
 
   for (var i = 0; i < numCPUs; i++) {
     var ENV_VAR = {}
-    if(i == 0) {
-    	ENV_VAR = { 'PRIMARY_WORKER': 1 }
+    if (i == 0) {
+      ENV_VAR = { 'PRIMARY_WORKER': 1 }
     }
 
     var worker = cluster.fork(ENV_VAR);
   }
 
-  cluster.on('exit', function(worker, code, signal) {
+  cluster.on('exit', function (worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died');
   });
 
