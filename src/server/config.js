a1e72850fc928a0ffada4e1a9e8208797aa0d47d
luizmlpascoal <luizmlpascoal@gmail.com>
2020-11-17 23:35:03 -0300

    ndvi domains

--- src/server/config.js
+++ src/server/config.js
@@ -35,8 +35,8 @@ module.exports = function (app) {
 		},
 		"port": env.PORT,
 		"ows_host": env.OWS_HOST,
-		"lapig-maps": env.LAPIG_MAPS,
-		"ows_domains": env.OWS_DOMAINS
+		"ows_domains": env.OWS_DOMAINS,
+		"ndvi_domain": env.NDVI_DOMAIN
 
 	};
 
@@ -59,6 +59,7 @@ module.exports = function (app) {
 		config["uploadDataDir"] = appProducao + env.UPLOAD_DATA_DIR;
 		config["downloadDataDir"] = appProducao + env.DOWNLOAD_DATA_DIR;
 		config["ows_domains"] = env.OWS_DOMAINS;
+		config["ndvi_domain"] = env.NDVI_DOMAIN;
 
 	}
 
