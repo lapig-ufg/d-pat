44b7fc64acdce59f1662eb94f2d0b42072c23617
FernandaStefani <fer.stefani.souza@gmail.com>
2021-01-21 12:40:03 -0300

    Update URL Mapbox

--- src/client/src/app/views/map.component.ts
+++ src/client/src/app/views/map.component.ts
@@ -1493,7 +1493,7 @@ export class MapComponent implements OnInit, AfterViewChecked {
         source: new OlXYZ({
           wrapX: false,
           url:
-            'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+            'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
         }),
         visible: true
       })
